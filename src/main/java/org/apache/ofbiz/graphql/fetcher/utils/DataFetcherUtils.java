82376ae06339796e1d8062882d0ec3e3630bac4c
Girish <girish.vasmatkar@hotwaxsystems.com>
2020-04-01 22:56:56 +0530

    Fixed unused import

--- src/main/java/org/apache/ofbiz/graphql/fetcher/utils/DataFetcherUtils.java
+++ src/main/java/org/apache/ofbiz/graphql/fetcher/utils/DataFetcherUtils.java
@@ -18,7 +18,6 @@
  *******************************************************************************/
 package org.apache.ofbiz.graphql.fetcher.utils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
